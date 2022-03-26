5a71ce50deaa17947e8b49380d1b70d3b590799f
CamiloEspindola <cespindola@unal.edu.co>
2022-03-26 12:40:23 -0500

    Create TV.java

---
+++ src/test/java/test/TV.java
@@ -0,0 +1,84 @@
+package taller3.televisores;
+
+
+
+public class TV {
+	private Marca marca;
+	private int canal = 1;
+	private int precio = 500;
+	private boolean estado;
+	private int volumen = 1;
+    private Control control;
+    public static int numTV;
+	
+    public TV(Marca marca, boolean estado ) {
+		this.marca = marca;
+	    this.estado = estado;
+	}
+    // setters
+    public void setMarca(Marca marca) {
+    	this.marca = marca;
+    }
+    public void setControl(Control control) {
+    	this.control = control;
+    }
+    public void setPrecio(int precio) {
+    	this.precio = precio;
+    }
+    public void setVolumen(int volumen) {
+    	this.volumen = volumen;
+    }
+    public void setCanal(int canal) {
+    	this.canal = canal;
+    }
+    public void setNumTV(int num) {
+    	numTV = num;
+    }
+    //getters
+    public Marca getMarca() {
+    	return marca;
+    }
+    public Control getControl() {
+    	return control;
+    }
+    public int getPrecio() {
+    	return precio;
+    }
+    public int getVolumen() {
+    	return volumen;
+    }
+    public int getCanal() {
+    	return canal;
+    }
+    public int getNumTv() {
+    	return numTV;
+    }
+    public void turnOn() {
+    	estado = true;
+    }
+    public void turnOff() {
+    	estado = false;
+    }
+    
+    public boolean getEstado() {
+    	return estado;
+    }
+    public void canalUp() {
+    	if (estado == true && canal<119)
+    	canal++;
+    }
+    public void canalDown() {
+    	if (estado == true && canal>1)
+    	canal--;
+    }
+    public void volumenUp() {
+    	if (estado == true && volumen<6)
+    	volumen++;
+    }
+    public void volumenDown() {
+    	if (estado == true && volumen>1)
+    	volumen--;
+    }    
+}
+
+
